615f66c00abf90131bb50fc095b93b05d9713224
OlegAlexandrov <Oleg.Alexandrov@quest.com>
2016-10-31 15:26:11 +0000

    move fit tests result to s3 bucket

--- handler.js
+++ handler.js
@@ -1,7 +1,24 @@
 
+function uploadFileToS3(filePath, event, cb){ 
+    cb('uploadFileToS3');
+    var AWS = require('aws-sdk');
+    var s3 = new AWS.S3();
+    var file = require('fs').createReadStream(filePath);
+    var params = {Bucket: event.bucket, Key: event.test + ".xml", Body: file};
+    cb(params);
+    s3.upload(params, function(err, data) {
+        if (err) console.log(err, err.stack); // an error occurred
+        else console.log(data);           // successful response
+
+        cb('event.test + ".xml" move to S3 to event.bucket');
+
+	});        
+}
+
 function runTest(event, context, cb){ 
 
 	const exec = require('child_process').exec;
+
     exec("export API_GATEWAY=" + event.apy_gateway + "; cp -a FitNesseRoot /tmp; chmod 755 /tmp; java -jar fitnesse-standalone.jar -d '/tmp' -c '" + event.test + "?test&format=xml&includehtml' -b '/tmp/" + event.test + ".xml' -p 8003; cat '/tmp/" + event.test + ".xml'", (error, stdout, stderr) => {
 	  if (error) {
 	    console.error(`exec error: ${error}`);
@@ -9,7 +26,8 @@ function runTest(event, context, cb){
 	  }
 	  console.log(`stdout: ${stdout}`);
 	  console.log(`stderr: ${stderr}`);
-          cb(null, { ok: true })
+      uploadFileToS3("/tmp/" + event.test + ".xml", event, console.log)
+      cb(null, { ok: true })
 	});
 
 };
